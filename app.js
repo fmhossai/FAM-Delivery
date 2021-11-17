64d29a3341c4f7cc030354a1ee96dedbf340c17d
MikArmy <esercitatomichele@gmail.com>
2021-11-17 14:06:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+
+const routes = require("./routes");
+const app = express();
+
+var server = {
+    port: 3001
+};
+
+app.set("port", process.env.PORT || server.port);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(routes);
+
+app.listen(app.get("port"), function() {
+    console.log("Server started on port " + app.get("port"));
+});

df7f50c075457d68acedac14e30a6d65113982d6
MikArmy <esercitatomichele@gmail.com>
2021-11-23 00:26:26 -0700

    added additional routes and pages

--- routes.js
+++ routes.js
@@ -1,10 +1,28 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res){
-    console.log("Accessed strating page.");
+// index page
+router.get("/", function(req, res) {
+    console.log("Accessed index page.");
     res.render("index");
 });
 
+// login page
+router.get("/login", function(req, res) {
+    console.log("Accessed login page.");
+    res.render("login");
+});
+
+// signup page
+router.get("/signup", function(req, res) {
+    console.log("Accessed signup page.");
+    res.render("signup");
+});
+
+// about page
+router.get("/about", function(req, res) {
+    console.log("Accessed about page.");
+    res.render("about");
+});
 
 module.exports = router;

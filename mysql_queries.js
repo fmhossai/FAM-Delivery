9004e2dca9829e20f8814d737d468350040c9e88
MikArmy <esercitatomichele@gmail.com>
2021-12-13 01:06:36 -0700

    more queries completed

--- mysql_queries.js
+++ mysql_queries.js
@@ -33,7 +33,7 @@ async function getCategories() {
 }
 
 async function getCart(username) {
-    const cartQuery = "SELECT product_id, pname, price, category, image_link \
+    const cartQuery = "SELECT product_id, pname, price, category, qty, image_link \
     FROM customer JOIN account ON customer_id = id NATURAL JOIN cart NATURAL JOIN product \
     WHERE username = ?";
 
@@ -42,12 +42,42 @@ async function getCart(username) {
 
 // exists queries to check if an account with a certain username already exists
 async function usernameExists(username) {
-    const cartQuery = "SELECT 1 \
+    const userQuery = "SELECT 1 \
     FROM account \
     WHERE username = ?";
-    let res = await query(cartQuery, [username]);
+    let res = await query(userQuery, [username]);
     return (res.length) ? true : false;
 }
+
+/**
+ * @param {*} username 
+ * @returns account id with the corresponding username
+ */
+ async function getAccountId(username) {
+    const userQuery = "SELECT id \
+    FROM account \
+    WHERE username = ?";
+    
+    return await query(userQuery, [username]);
+}
+
+/**
+ * if the product is already in the cart qty will be increased by 1
+ * @param {*} username username of the cart holder
+ * @param {*} productId product id of item to be added to the cart
+ * @returns 
+ */
+async function addToCart(username, productId) {
+    const cartQuery = "INSERT INTO cart (customer_id, product_id, qty) VALUES (?, ?, 1) \
+    ON DUPLICATE KEY UPDATE qty = qty + 1";
+
+    let accountId = await getAccountId(username);
+
+    await query(cartQuery, [accountId[0].id, productId]);
+    return true;
+}
+
+
 
 async function display() {
     // console.log(await getProducts());
@@ -56,6 +86,8 @@ async function display() {
     // console.log(await getCategories());
     // console.log(await getCart("demoCustomer"));
     // console.log(await usernameExists("demoCustomer"));
+    // console.log(await getAccountId("demoCustomer"));
+    // await addToCart("demoCustomer", 2);
 }
 
 display();
@@ -66,3 +98,5 @@ module.exports.getCategorizedProducts = getCategorizedProducts;
 module.exports.getCategories = getCategories;
 module.exports.getCart = getCart;
 module.exports.usernameExists = usernameExists;
+module.exports.getAccountId = getAccountId;
+module.exports.addToCart = addToCart;

88bb6dcf4234105e3854bc2dbabc757c41793f06
MikArmy <esercitatomichele@gmail.com>
2021-12-13 20:18:42 -0700

    db schema updated and more queries added

--- app/models/mysql_queries.js
+++ app/models/mysql_queries.js
@@ -53,7 +53,7 @@ async function usernameExists(username) {
  * @param {*} username 
  * @returns account id with the corresponding username
  */
- async function getAccountId(username) {
+async function getAccountId(username) {
     const userQuery = "SELECT id \
     FROM account \
     WHERE username = ?";
@@ -77,7 +77,167 @@ async function addToCart(username, productId) {
     return true;
 }
 
+/**
+ * @param {*} name account name
+ * @param {*} username account username - must be unique
+ * @param {*} email account email
+ * @param {*} password account password
+ * @returns id of account
+ */
+async function addAccount(name, username, email, password) {
+    const userQuery = "INSERT INTO account (name, username, email, password) VALUES (?,?,?,?)";
 
+    await query(userQuery, [name, username, email, password]);
+    return await getAccountId(username);
+}
+
+/**
+ * @param {*} name customer name
+ * @param {*} username customer username - must be unique
+ * @param {*} email customer email
+ * @param {*} password customer password
+ * @returns
+ */
+async function addCustomerAccount(name, username, email, password) {
+    const userQuery = "INSERT INTO customer (customer_id) VALUES (?)";
+
+    let accountId = await addAccount(name, username, email, password);
+
+    await query(userQuery, [accountId[0].id]);
+    return true;
+}
+
+/**
+ * @param {*} name supplier name
+ * @param {*} username supplier username - must be unique
+ * @param {*} email supplier email
+ * @param {*} password supplier password
+ * @returns
+ */
+async function addSupplierAccount(name, username, email, password) {
+    const userQuery = "INSERT INTO supplier (supplier_id) VALUES (?)";
+
+    let accountId = await addAccount(name, username, email, password);
+
+    await query(userQuery, [accountId[0].id]);
+    return true;
+}
+
+// add/remove categories of supplier
+async function addSupplierCategory(username, category) {
+    const categoryQuery = "INSERT INTO category (supplier_id, category) VALUES (?,?)";
+
+    let accountId = await getAccountId(username);
+
+    await query(categoryQuery, [accountId[0].id, category]);
+    return true;
+}
+
+async function removeSupplierCategory(username, category) {
+    const categoryQuery = "DELETE FROM category \
+    WHERE supplier_id = ? AND \
+    category = ?";
+
+    let accountId = await getAccountId(username);
+
+    await query(categoryQuery, [accountId[0].id, category]);
+    return true;
+}
+
+// get account for cutomer or supplier
+async function getCustomer(username) {
+    const userQuery = "SELECT * \
+    FROM account INNER JOIN customer ON id = customer_id \
+    WHERE username = ?";
+
+    return await query(userQuery, [username]);
+}
+
+async function getSupplier(username) {
+    const userQuery = "SELECT * \
+    FROM account INNER JOIN supplier ON id = supplier_id \
+    WHERE username = ?";
+
+    return await query(userQuery, [username]);
+}
+
+/**
+ * @param {*} username username of target user for change
+ * @param {*} name field of change (string)
+ * @returns 
+ */
+async function setName(username, name) {
+    const nameQuery = "UPDATE account \
+        SET name = ? \
+        WHERE username = ?";
+
+    await query(nameQuery, [name, username]);
+    return true;
+}
+
+/**
+ * @param {*} username username of target user for change
+ * @param {*} email field of change (string)
+ * @returns 
+ */
+async function setEmail(username, email) {
+    const emailQuery = "UPDATE account \
+        SET email = ? \
+        WHERE username = ?";
+
+    await query(emailQuery, [email, username]);
+    return true;
+}
+
+/**
+ * @param {*} username username of target user for change
+ * @param {*} password field of change (string)
+ * @returns 
+ */
+async function setPassword(username, password) {
+    const passwordQuery = "UPDATE account \
+        SET password = ? \
+        WHERE username = ?";
+
+    await query(passwordQuery, [password, username]);
+    return true;
+}
+
+/**
+ * @param {*} username username of target user for change
+ * @param {*} phoneNo field of change (string)
+ * @returns 
+ */
+async function setPhoneNo(username, phoneNo) {
+    const phoneQuery = "UPDATE account \
+        SET phone_no = ? \
+        WHERE username = ?";
+
+    await query(phoneQuery, [phoneNo, username]);
+    return true;
+}
+
+/**
+ * @param {*} username username of target user for change
+ * @param {*} streetNo field of change (+ve int)
+ * @param {*} streetName field of change (string)
+ * @param {*} postalCode field of change (string)
+ * @param {*} city field of change (string)
+ * @param {*} country field of change (string)
+ * @returns 
+ */
+async function setAddress(username, streetNo, streetName, postalCode, city, country) {
+    const addressQuery = "UPDATE account \
+        SET address_street_no = ?, \
+            address_street_name = ?, \
+            address_postal_code = ?, \
+            address_city = ?, \
+            address_country = ? \
+        WHERE username = ?";
+
+    await query(addressQuery, [streetNo, streetName, postalCode, city, country, username]);
+    return true;
+}
 
 async function display() {
     // console.log(await getProducts());
@@ -88,6 +248,8 @@ async function display() {
     // console.log(await usernameExists("demoCustomer"));
     // console.log(await getAccountId("demoCustomer"));
     // await addToCart("demoCustomer", 2);
+    // await setName("demoCustomer", "Bob The Great");
+    // await setAddress("demoCustomer", 77, "Awesome Street", "H8V 6G3", "Calgary", "Canada");
 }
 
 display();
@@ -100,3 +262,15 @@ module.exports.getCart = getCart;
 module.exports.usernameExists = usernameExists;
 module.exports.getAccountId = getAccountId;
 module.exports.addToCart = addToCart;
+module.exports.addAccount = addAccount;
+module.exports.addCustomerAccount = addCustomerAccount;
+module.exports.addSupplierAccount = addSupplierAccount;
+module.exports.addSupplierCategory = addSupplierCategory;
+module.exports.removeSupplierCategory = removeSupplierCategory;
+module.exports.getCustomer = getCustomer;
+module.exports.getSupplier = getSupplier;
+module.exports.setName = setName;
+module.exports.setEmail = setEmail;
+module.exports.setPassword = setPassword;
+module.exports.setPhoneNo = setPhoneNo;
+module.exports.setAddress = setAddress;

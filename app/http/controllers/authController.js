1ff09ce4b7a6eca414cd442a1df02ebdc80fa2d4
Ahmed <ahmedamr12304@gmail.com>
2021-12-13 21:42:34 -0700

    fix login

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,39 +1,49 @@
 const bcrypt = require('bcrypt');
+const { usernameExists } = require('../../models/mysql_queries');
 
 function authController() {
     return {
         login(req, res) {
-            res.render('login')
+            res.render('login', { login: true})
         },
         async postLogin(req, res) {
-            /*const { name, username, email, password } = req.body
+            //sign up
+            if(Object.keys(req.body).length === 4 ){
+                const { name, username, email, password } = req.body;
+                if(!name || !email || !username || !password){
+                    req.flash('errorSignUp', '1 or more invalid fields');
+                    req.flash('name', name);
+                    req.flash('username', username);
+                    req.flash('email', email);
+                    return res.render('login', { login: false });
+                }
 
-            //need to restructure
-            if(!name || !email || !username || !password){
-                req.flash('error', '1 or more invalid fields');
-                req.flash('name', name);
-                req.flash('email', email);
-                return res.redirect('/login');
+                //check if username is in database
+                const exists = await usernameExists(username);
+                
+                //if it does will display error and redirect to register page again and if not do what is below
+                if(exists){
+                    req.flash('errorSignUp', 'Username Taken');
+                    req.flash('name', name);
+                    req.flash('username', username);
+                    req.flash('email', email);
+                    return res.render('login');
+                }else {
+                    //hash password
+                    const hashedPassword = await bcrypt.hash(password, 10);
+
+                    //create a user
+                    //redirect to home page
+                }
+            //log in
+            }else if(Object.keys(req.body).length === 2){
+                const { username, password } = req.body;
+                if(!username || !password){
+                    req.flash('errorLogIn', '1 or more invalid fields');
+                    req.flash('username', username);
+                    return res.render('login', { login: true });
+                }
             }
-
-            //check if email/phone exits
-
-
-            //if it does will display error and redirect to register page agin and if not do what is below
-
-            //optional: hash password
-            const hashedPassword = await bcrypt.hash(password, 10);
-
-            //
-
-            //create a usser
-
-
-            //
-
-            //redirect to home page*/
-
-            console.log(req.body)
         }
     }
 }
--- public/src/login.js
+++ public/src/login.js
@@ -1,4 +1,3 @@
-
 let switchCtn = document.querySelector("#alternate");
 let switchC1 = document.querySelector("#altLogin");
 let switchC2 = document.querySelector("#altSignUp");
@@ -6,8 +5,35 @@ let switchBtn = document.querySelectorAll(".alternateBtn");
 let aContainer = document.querySelector("#signUpContainer");
 let bContainer = document.querySelector("#loginContainer");
 let allButtons = document.querySelectorAll(".submit");
+let login = document.querySelector(".login").getAttribute("data");
 
 let getButtons = (e) => e.preventDefault()
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log(login)
+    /*
+    if(!login) {
+        switchCtn.classList.toggle("is-txr");
+        switchC1.classList.toggle("is-hidden");
+        switchC2.classList.toggle("is-hidden");
+        aContainer.classList.toggle("is-txl");
+        bContainer.classList.toggle("is-txl");
+        bContainer.classList.toggle("is-z200");
+    }
+    */
+   if(login){
+        switchC1.classList.remove("is-hidden");
+        switchC2.classList.add("is-hidden");
+        aContainer.classList.remove("is-txl");
+        bContainer.classList.add("is-z200");
+   }else {
+        switchC1.classList.add("is-hidden");
+        switchC2.classList.remove("is-hidden");
+        aContainer.classList.add("is-txl");
+        bContainer.classList.remove("is-z200");
+   }
+});
+
 
 let changeForm = (e) => {
 
@@ -16,13 +42,12 @@ let changeForm = (e) => {
         switchCtn.classList.remove("is-gx");
     }, 1500)
 
-    switchCtn.classList.toggle("is-txr");
-
-    switchC1.classList.toggle("is-hidden");
-    switchC2.classList.toggle("is-hidden");
-    aContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-txl");
-    bContainer.classList.toggle("is-z200");
+        switchCtn.classList.toggle("is-txr");
+        switchC1.classList.toggle("is-hidden");
+        switchC2.classList.toggle("is-hidden");
+        aContainer.classList.toggle("is-txl");
+        bContainer.classList.toggle("is-txl");
+        bContainer.classList.toggle("is-z200");
 }
 
 let mainF = (e) => {

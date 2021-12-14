22613e49874cae0caf3b8db16464d8f157e5f4a7
Faisal Hossain <45044819+fmhossai@users.noreply.github.com>
2021-12-13 20:21:54 -0700

    Added search

--- public/src/search.js
+++ public/src/search.js
@@ -3,5 +3,7 @@ let searchBar = document.querySelector(".search").firstElementChild
 searchBar.addEventListener("keydown", (e)=>{
     if(e.key == "Enter"){
         console.log(e.target.value)
+        let params = new URLSearchParams({string: e.target.value});
+        window.location.replace(`/search?${params.toString()}`)
     }
 })

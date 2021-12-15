088a7c8a9a8a99ff7763da3aba8c69cfe02cbc43
Ahmed <ahmedamr12304@gmail.com>
2021-12-14 19:59:41 -0700

    sign out complete

--- app/http/controllers/logoutController.js
+++ app/http/controllers/logoutController.js
@@ -3,6 +3,7 @@ function logoutController() {
         index(req,res){
            if(req.session.username){
                req.session.username = '';
+               req.session.cart = '';
                return res.redirect('/');
            }else {
                return res.redirect('/');
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const cartController = require('../app/http/controllers/cartController');
 const homeController = require('../app/http/controllers/homeController');
 const categoriesController = require('../app/http/controllers/categoriesController');
 const searchController = require('../app/http/controllers/searchController')
-const logoutController = require('../app/http/controllers/logoutController')
+const logoutController = require('../app/http/controllers/logoutController');
+const profileController = require('../app/http/controllers/profileController');
 function initRoutes(app, axios) {
 
     app.get('/', homeController(axios).index);
@@ -11,7 +12,7 @@ function initRoutes(app, axios) {
     app.get('/login', authController().login);
     app.post('/login', authController().postLogin);
 
-    app.get('/logout', logoutController().index)
+    app.get('/logout', logoutController().index);
 
     app.get('/cart', cartController().index);
     app.get('/search', searchController().index)
@@ -19,9 +20,7 @@ function initRoutes(app, axios) {
 
     app.get('/categories/:category', categoriesController().index);
 
-    app.get('/profile', (req,res) => {
-        res.render('profile');
-    })
+    app.get('/profile', profileController().index);
 
     app.get('/checkout', (req, res) => {
         res.render('checkout');

5d991858a226b14e978ab6c78a90741571d17483
Ahmed <ahmedamr12304@gmail.com>
2021-12-17 00:08:08 -0700

    rating modal done

--- public/src/index.js
+++ public/src/index.js
@@ -10,5 +10,16 @@ card.addEventListener('click', (e) => {
 closeBtn.addEventListener('click', (e) => {
     blur.classList.toggle('active');
     document.getElementById('modal').classList.toggle('active');
-})
+});
 
+document.querySelector('.reviewBtn').addEventListener('click', (e) => {
+    let description = document.querySelector(".ta").value;
+    let selected;
+
+    if(document.getElementById('rate-5').checked) selected = document.getElementById("rate-5").value;
+    else if(document.getElementById('rate-4').checked) selected = document.getElementById("rate-4").value;
+    else if(document.getElementById('rate-3').checked) selected = document.getElementById("rate-3").value;
+    else if(document.getElementById('rate-2').checked) selected = document.getElementById("rate-2").value;
+    else if(document.getElementById('rate-1').checked) selected = document.getElementById("rate-1").value;
+    console.log(selected, description);
+})

06f06ea0b361d5d15b4ce8382ba29acc18407f34
Ahmed <ahmedamr12304@gmail.com>
2021-12-16 23:12:38 -0700

    ratings started

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,14 @@
-const card = document.querySelector('.card')
+const card = document.querySelector('.card');
+const blur = document.getElementById('blur');
+const closeBtn = document.querySelector('.closeBtn');
+
 card.addEventListener('click', (e) => {
-    
-    var blur = document.getElementById('blur');
     blur.classList.toggle('active');
-    var modal = document.getElementById('modal');
-    modal.classList.toggle('active');
+    document.getElementById('modal').classList.toggle('active');
 });
+
+closeBtn.addEventListener('click', (e) => {
+    blur.classList.toggle('active');
+    document.getElementById('modal').classList.toggle('active');
+})
+

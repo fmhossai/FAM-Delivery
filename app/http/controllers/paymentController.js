26a62791598787ccbbe7a305681819356d49e4fd
Faisal Hossain <45044819+fmhossai@users.noreply.github.com>
2021-12-17 20:10:42 -0700

    Added initial payment storing

--- app/http/controllers/paymentController.js
+++ app/http/controllers/paymentController.js
@@ -4,7 +4,20 @@ function paymentController(){
     return{
         async index(req, res){
             console.log("Payment success")
+            console.log({
+                card_name: req.body.card_name,
+                card_number: req.body.card_number,
+                card_date: req.body.card_number,
+                card_cvv: req.body.card_cvv
+            })
             const itemsInCart = Object.values(req.session.cart.items);
+            let productIds = []
+            for(const i of itemsInCart){
+                productIds.push({
+                    product_id: i.item.product_id,
+                    qty: i.qty
+                });
+            }
             for(const i of itemsInCart){
                 await decreaseProductStock(i.item.product_id, i.qty)
                 await removeCartItem(req.session.username, i.item.product_id)
